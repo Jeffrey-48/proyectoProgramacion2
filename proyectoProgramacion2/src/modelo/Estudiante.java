82856927abe275b2685013638bb91ef5e4489cdb
brayand1998 <69978418+brayand1998@users.noreply.github.com>
2021-12-09 18:56:11 -0500

    Clase Profesor, Estudiante

---
+++ proyectoProgramacion2/src/modelo/Estudiante.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+ * @author braya
+ *
+ */
+public class Estudiante extends Persona{
+
+	public int semestre;
+	public int codigoEstudiante;
+	/**
+	 * @param nombre
+	 * @param apellido
+	 * @param correo
+	 * @param direccion
+	 * @param identificador
+	 * @param telefono
+	 * @param tipoSangre
+	 * @param rh
+	 * @param fechaNacimiento
+	 * @param genero
+	 * @param tipoVinculacion
+	 * @param semestre
+	 * @param codigoEstudiante
+	 */
+	public Estudiante(String nombre, String apellido, String correo, String direccion, String identificador,
+			String telefono, String tipoSangre, String rh, String fechaNacimiento, String genero,
+			String tipoVinculacion, int semestre, int codigoEstudiante) {
+		super(nombre, apellido, correo, direccion, identificador, telefono, tipoSangre, rh, fechaNacimiento, genero,
+				tipoVinculacion);
+		this.semestre = semestre;
+		this.codigoEstudiante = codigoEstudiante;
+	}
+	
+	public Estudiante()
+	{
+		
+	}
+
+	/**
+	 * @return the semestre
+	 */
+	public int getSemestre() {
+		return semestre;
+	}
+
+	/**
+	 * @param semestre the semestre to set
+	 */
+	public void setSemestre(int semestre) {
+		this.semestre = semestre;
+	}
+
+	/**
+	 * @return the codigoEstudiante
+	 */
+	public int getCodigoEstudiante() {
+		return codigoEstudiante;
+	}
+
+	/**
+	 * @param codigoEstudiante the codigoEstudiante to set
+	 */
+	public void setCodigoEstudiante(int codigoEstudiante) {
+		this.codigoEstudiante = codigoEstudiante;
+	}
+	
+	
+	
+	
+}
---
+++ proyectoProgramacion2/src/modelo/Profesor.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+ * @author braya
+ *
+ */
+public class Profesor extends Persona{
+	public String especialidad;
+
+	/**
+	 * @param nombre
+	 * @param apellido
+	 * @param correo
+	 * @param direccion
+	 * @param identificador
+	 * @param telefono
+	 * @param tipoSangre
+	 * @param rh
+	 * @param fechaNacimiento
+	 * @param genero
+	 * @param tipoVinculacion
+	 * @param especialidad
+	 */
+	public Profesor(String nombre, String apellido, String correo, String direccion, String identificador,
+			String telefono, String tipoSangre, String rh, String fechaNacimiento, String genero,
+			String tipoVinculacion, String especialidad) {
+		super(nombre, apellido, correo, direccion, identificador, telefono, tipoSangre, rh, fechaNacimiento, genero,
+				tipoVinculacion);
+		this.especialidad = especialidad;
+	}
+	
+	/**
+	 * 
+	 */
+	public Profesor()
+	{
+		
+	}
+
+	/**
+	 * @return the especialidad
+	 */
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	/**
+	 * @param especialidad the especialidad to set
+	 */
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+	
+	
+
+}
