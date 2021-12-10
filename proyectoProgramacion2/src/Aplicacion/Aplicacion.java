095d90f2816362d527ef29ca345cc154243eece2
brayand1998 <69978418+brayand1998@users.noreply.github.com>
2021-12-09 19:18:18 -0500

    Paquete Aplicacion

---
+++ proyectoProgramacion2/src/Aplicacion/Aplicacion.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Aplicacion;
+
+/**
+ * @author braya
+ *
+ */
+public class Aplicacion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
