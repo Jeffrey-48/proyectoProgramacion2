f2f0890dd09c698de97056ef30ae59b67b154f33
brayand1998 <69978418+brayand1998@users.noreply.github.com>
2021-12-09 19:07:40 -0500

    Clase Programa

---
+++ proyectoProgramacion2/src/modelo/Programa.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+ * @author braya
+ *
+ */
+public class Programa {
+	
+	public String nombre;
+	public int creditos;
+	public int numeroEstudiantesRegistrados;
+	/**
+	 * @param nombre
+	 * @param creditos
+	 * @param numeroEstudiantesRegistrados
+	 */
+	public Programa(String nombre, int creditos, int numeroEstudiantesRegistrados) {
+		super();
+		this.nombre = nombre;
+		this.creditos = creditos;
+		this.numeroEstudiantesRegistrados = numeroEstudiantesRegistrados;
+	}
+	
+	public Programa() {
+		
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the creditos
+	 */
+	public int getCreditos() {
+		return creditos;
+	}
+
+	/**
+	 * @param creditos the creditos to set
+	 */
+	public void setCreditos(int creditos) {
+		this.creditos = creditos;
+	}
+
+	/**
+	 * @return the numeroEstudiantesRegistrados
+	 */
+	public int getNumeroEstudiantesRegistrados() {
+		return numeroEstudiantesRegistrados;
+	}
+
+	/**
+	 * @param numeroEstudiantesRegistrados the numeroEstudiantesRegistrados to set
+	 */
+	public void setNumeroEstudiantesRegistrados(int numeroEstudiantesRegistrados) {
+		this.numeroEstudiantesRegistrados = numeroEstudiantesRegistrados;
+	}
+	
+	
+
+}
