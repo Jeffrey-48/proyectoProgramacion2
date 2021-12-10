39b74152f0659a3a5716b3623f5acc752650c393
brayand1998 <69978418+brayand1998@users.noreply.github.com>
2021-12-09 20:36:02 -0500

    Creacion Profesor

--- proyectoProgramacion2/src/Logica/Principal.java
+++ proyectoProgramacion2/src/Logica/Principal.java
@@ -3,10 +3,12 @@ package Logica;
 import java.util.ArrayList;
 
 import modelo.Estudiante;
+import modelo.Profesor;
 
 public class Principal {
 	
 	public ArrayList<Estudiante> estudiantes;
+	public ArrayList<Profesor> profesor;
 	
 	public Principal() {
 		iniciar();
@@ -15,6 +17,8 @@ public class Principal {
 	private void iniciar() {
 		CreacionEstudiante estudiantes = new CreacionEstudiante();
 		this.estudiantes=estudiantes.getEstudiantesInscritos();
+		CreacionProfesor profesor = new CreacionProfesor();
+		this.profesor = profesor.getMisProfesores();
 	}
 	
 }
