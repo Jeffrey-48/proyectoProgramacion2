ed7f31b980bbb68cbcff1d580114b25fea60b346
brayand1998 <69978418+brayand1998@users.noreply.github.com>
2021-12-09 19:02:12 -0500

    Clase Facultad

---
+++ proyectoProgramacion2/src/modelo/Facultad.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+ * @author braya
+ *
+ */
+public class Facultad {
+	
+	public String nombre;
+	public int numeroProgramas;
+	/**
+	 * @param nombre
+	 * @param numeroProgramas
+	 */
+	public Facultad(String nombre, int numeroProgramas) {
+		super();
+		this.nombre = nombre;
+		this.numeroProgramas = numeroProgramas;
+	}
+
+	public Facultad(){
+		
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the numeroProgramas
+	 */
+	public int getNumeroProgramas() {
+		return numeroProgramas;
+	}
+
+	/**
+	 * @param numeroProgramas the numeroProgramas to set
+	 */
+	public void setNumeroProgramas(int numeroProgramas) {
+		this.numeroProgramas = numeroProgramas;
+	}
+	
+	
+}
